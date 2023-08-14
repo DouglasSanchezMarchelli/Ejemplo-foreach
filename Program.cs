dca1bdc6b2e6b8f538aded595ecdc19ddd428624
Douglas Sánchez <142137936+DouglasSanchezMarchelli@users.noreply.github.com>
2023-08-14 15:35:53 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EjemploForeach
+{
+class Program
+{
+ static void Main(string[] args)
+ {
+ Console.WriteLine("Ejemplo de Uso de foreach");
+ Console.WriteLine("_________________________");
+
+  // Crear una lista de números
+  List<int> numeros = new List<int> {10,20,30,40,50 };
+
+ // Usar foreach para recorrer la lista
+  foreach (int numero in numeros)
+  {
+  Console.WriteLine(numero);
+  }
+  }
+ }
+}
